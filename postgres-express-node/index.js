632c0b4023a5ddc233ae3a282d48a905e180416f
DinoZzo99 <dinozoricic99@gmail.com>
2021-01-12 14:41:07 +0100

    2 premissions added

--- postgres-express-node/index.js
+++ postgres-express-node/index.js
@@ -62,9 +62,22 @@ const MedicalTest_5 = subject("MedicalTest", {
 // * =============================
 // *  Roles <--> permissions
 // * -----------------------------
-function defineAdminRules({ can }, user) {}
+function defineAdminRules({ can }, user) {
+  can('manage','all');
+}
+
 function defineDoctorRules({ can }, user) {}
-function defineUserRules({ can }, user) {}
+
+function defineUserRules({ can }, user) {
+  // can('read', 'MedicalTest', {UserId: user.id});
+  // can('create', 'MedicalTest', {UserId: user.id});
+  // can('update', 'MedicalTest', {UserId: user.id});
+  // can('delete', 'MedicalTest', {UserId: user.id});
+  can(['read','create','delete','update'],'MedicalTest', {
+    UserId: user.id
+  });
+}
+
 function defineAnonymousRules({ can }, user) {}
 
 // * =============================
